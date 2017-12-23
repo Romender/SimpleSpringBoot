e4907fdef964dd82bde3238333af7b9f28a7d7b5
romender <romender.singh@gmail.com>
2017-12-23 09:00:17 -0800

    Created Hello World Application.

---
+++ src/main/java/org/learn/application/Application.java
@@ -0,0 +1,14 @@
+package org.learn.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@ComponentScan("org.learn")
+@SpringBootApplication
+public class Application {
+    public static void main(String args[] ){
+        SpringApplication.run(Application.class,args);
+    }
+}
