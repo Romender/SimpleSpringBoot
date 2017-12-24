feef42e69a4688a8d640f51d8465e9580552392d
romender <romender.singh@gmail.com>
2017-12-24 00:35:46 -0800

    Added the manifiest.yml and added application plugin in the build.gradle.

--- src/main/java/org/learn/controller/HelloWorldController.java
+++ src/main/java/org/learn/controller/HelloWorldController.java
@@ -1,8 +1,6 @@
 package org.learn.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
